81b87bf11b72927e74b54275382d596888dbe2a6
uglov <mic-uglov@ya.ru>
2022-11-28 12:25:36 +0300

    feat: primary key instead of unique in likes, friends and film_genre

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,14 +25,14 @@ CREATE TABLE IF NOT EXISTS films (
 CREATE TABLE IF NOT EXISTS film_genre (
     film_id BIGINT NOT NULL REFERENCES films (id),
     genre_id BIGINT NOT NULL REFERENCES genres (id),
-    CONSTRAINT unique_film_genre UNIQUE (film_id, genre_id));
+    PRIMARY KEY (film_id, genre_id));
 
 CREATE TABLE IF NOT EXISTS likes (
     film_id BIGINT NOT NULL REFERENCES films (id),
     user_id BIGINT NOT NULL REFERENCES users (id),
-    CONSTRAINT unique_likes UNIQUE (film_id, user_id));
+    PRIMARY KEY (film_id, user_id));
 
 CREATE TABLE IF NOT EXISTS friends (
     user_id BIGINT NOT NULL REFERENCES users (id),
     friend_id BIGINT NOT NULL REFERENCES users (id),
-    CONSTRAINT unique_friends UNIQUE (user_id, friend_id));
+    PRIMARY KEY (user_id, friend_id));
