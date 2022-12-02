5aa7d0a4b36c2e7603f6e2c554b96e4c8f1c12e4
uglov <mic-uglov@ya.ru>
2022-12-02 15:29:13 +0300

    fix: GenreStorage imports

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface GenreStorage {
